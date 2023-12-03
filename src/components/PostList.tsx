70e97eb9e66f0cb071f8cf20e7b99d1ae8e87c99
Concha Asensio <concha@codium.team>
2023-12-03 20:24:38 +0100

    Translate button names and remove unused elements

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -13,6 +13,7 @@ interface Props {
 
 export default function PropList({ posts, onRemove }: Props) {
   const navigate = useNavigate();
+
   const removePost = (id: number) => {
     axios.delete(`http://localhost:3000/api/v1/posts/${id}`).then((response) => {});
     onRemove(id);
@@ -22,16 +23,15 @@ export default function PropList({ posts, onRemove }: Props) {
     return (
       <li key={post.id}>
         <p>{post.title}</p>
-        <p>{post.content}</p>
-        <img src={post.image_url} alt={post.title} />
-        <button onClick={() => removePost(post.id)}>Borrar</button>
-        <button onClick={() => navigate(`/detail/${post.id}`)}>Más detalles</button>
+        <button onClick={() => removePost(post.id)}>Delete</button>
+        <button onClick={() => navigate(`/detail/${post.id}`)}>More details</button>
+        <button>Add post</button>
       </li>
     );
   });
 
   return (
-    <div className="App">
+    <div>
       <ul>{htmlCode}</ul>
     </div>
   );
