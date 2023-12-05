f8a3aaf69cb028dc846c2376fbf1ec89c032aaa8
Concha Asensio <concha@codium.team>
2023-12-05 18:06:48 +0100

    Test it renders and appears the post

---
+++ src/components/tests/ShowPost.test.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from '@testing-library/react';
+import ShowPost from '../ShowPost';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+
+it('Should render the page', () => {
+  render(
+    <MemoryRouter initialEntries={['/show/1']}>
+      <Routes>
+        <Route
+          path="/show/:id"
+          element={
+            <ShowPost
+              posts={[
+                {
+                  id: 1,
+                  title: 'Madrid',
+                  content: 'Madrid is the capital of Spain...',
+                  lat: '40.41678',
+                  long: '-3.70379',
+                  image_url: 'https://c2.staticflickr.com/2/1269/4670777817_d657cd9819_b.jpg',
+                },
+              ]}
+            />
+          }
+        />
+      </Routes>
+    </MemoryRouter>
+  );
+  const post = screen.getByText('Madrid');
+  expect(post).toBeInTheDocument();
+});
