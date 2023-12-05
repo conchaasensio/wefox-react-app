f8d5e42a11d6fd68e7bf59214653371b5243be25
Concha Asensio <concha@codium.team>
2023-12-05 20:38:41 +0100

    Test it renders the create post page

--- src/components/tests/CreatePost.test.tsx
+++ src/components/tests/CreatePost.test.tsx
@@ -1,7 +1,10 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import CreatePost from '../CreatePost';
 
 it('Should render the page', () => {
   render(<CreatePost />);
+
+  const createPostButton = screen.queryByRole('button', { name: 'Create Post' });
+  expect(createPostButton).toBeInTheDocument();
 });
