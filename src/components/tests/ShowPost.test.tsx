b8f1f101cd6e762fedc99a157c39ab74686f470d
Concha Asensio <concha@codium.team>
2023-12-05 18:18:05 +0100

    Refactor extract variable

--- src/components/tests/ShowPost.test.tsx
+++ src/components/tests/ShowPost.test.tsx
@@ -3,26 +3,18 @@ import ShowPost from '../ShowPost';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 
 it('Should render the page', () => {
+  const postData = {
+    id: 1,
+    title: 'Madrid',
+    content: 'Madrid is the capital of Spain...',
+    lat: '40.41678',
+    long: '-3.70379',
+    image_url: 'https://c2.staticflickr.com/2/1269/4670777817_d657cd9819_b.jpg',
+  };
   render(
     <MemoryRouter initialEntries={['/show/1']}>
       <Routes>
-        <Route
-          path="/show/:id"
-          element={
-            <ShowPost
-              posts={[
-                {
-                  id: 1,
-                  title: 'Madrid',
-                  content: 'Madrid is the capital of Spain...',
-                  lat: '40.41678',
-                  long: '-3.70379',
-                  image_url: 'https://c2.staticflickr.com/2/1269/4670777817_d657cd9819_b.jpg',
-                },
-              ]}
-            />
-          }
-        />
+        <Route path="/show/:id" element={<ShowPost posts={[postData]} />} />
       </Routes>
     </MemoryRouter>
   );
--- src/components/tests/UpdatePost.test.tsx
+++ src/components/tests/UpdatePost.test.tsx
@@ -3,26 +3,18 @@ import UpdatePost from '../UpdatePost';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 
 it('Should render the page', () => {
+  const postData = {
+    id: 1,
+    title: 'Madrid',
+    content: 'Madrid is the capital of Spain...',
+    lat: '40.41678',
+    long: '-3.70379',
+    image_url: 'https://c2.staticflickr.com/2/1269/4670777817_d657cd9819_b.jpg',
+  };
   render(
     <MemoryRouter initialEntries={['/update/1']}>
       <Routes>
-        <Route
-          path="/update/:id"
-          element={
-            <UpdatePost
-              posts={[
-                {
-                  id: 1,
-                  title: 'Madrid',
-                  content: 'Madrid is the capital of Spain...',
-                  lat: '40.41678',
-                  long: '-3.70379',
-                  image_url: 'https://c2.staticflickr.com/2/1269/4670777817_d657cd9819_b.jpg',
-                },
-              ]}
-            />
-          }
-        />
+        <Route path="/update/:id" element={<UpdatePost posts={[postData]} />} />
       </Routes>
     </MemoryRouter>
   );
