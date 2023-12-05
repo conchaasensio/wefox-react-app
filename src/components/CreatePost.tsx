064bb10b8467dff7eb9d36fd3becdc3905fe804b
Concha Asensio <concha@codium.team>
2023-12-05 10:09:54 +0100

    Improve form styles

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -34,12 +34,16 @@ const CreatePost = () => {
   return (
     <div>
       <form onSubmit={createPost}>
+        <label htmlFor="name">City name</label>
         <input onChange={updateInputValues} value={inputValues.title} type="text" name="title" placeholder="title" />
+        <label htmlFor="content">Content</label>
         <textarea onChange={updateInputValues} value={inputValues.content} name="content" placeholder="content" />
+        <label htmlFor="image">Image</label>
+        <input onChange={updateInputValues} value={inputValues.image_url} name="image_url" placeholder="image" />
+        <label htmlFor="latlong">Coordinates</label>
         <input onChange={updateInputValues} value={inputValues.lat} type="text" name="lat" placeholder="latitude" />
         <input onChange={updateInputValues} value={inputValues.long} type="text" name="long" placeholder="longitude" />
-        <input onChange={updateInputValues} value={inputValues.image_url} name="image_url" placeholder="image" />
-        <button>Create post</button>
+        <button>Create Post</button>
       </form>
     </div>
   );
