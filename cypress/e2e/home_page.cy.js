3f93e92fcbc2aac5ba15018320f4af65f1e7d7eb
Concha Asensio <concha@codium.team>
2023-12-06 06:52:36 +0100

    Test the page shows a list of post with cypress

--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -1,6 +1,11 @@
 describe('The Home Page', () => {
   it('successfully loads', () => {
     cy.visit('/');
-    cy.getByText('wefox');
+    cy.contains('wefox');
+  });
+
+  it('shows a list of posts', () => {
+    cy.visit('/');
+    cy.get('.MuiTypography-h5').should('have.length.gt', 1);
   });
 });
